4aff2e9aa28372c51adea7d68b8c9054e40f4f1d
Anson Lo <92462730+ansoncklo@users.noreply.github.com>
2023-01-15 13:29:26 +0800

    Add files via upload

---
+++ Landmarks/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Landmarks
+//
+//  Created by KEYTEO_HK_027 on 14/1/2023.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+    static let `default` = Profile(username: "chris_lo")
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { rawValue }
+    }
+}
